dffeac35eddbd37ef4cf026790b53f297b0494a8
MeloScav <meloscavezzoni@gmail.com>
2022-03-23 18:48:15 +0100

    Basic navbar

---
+++ src/views/_navbar.php
@@ -0,0 +1,27 @@
+<header class="header-nav">
+    <div class="container d-flex justify-centent-center">
+        <div class="logo-container">
+            <a class="navbar-brand" href="javascript:;">
+                <svg>
+                    <use class="icon-anim" href="../assets/sprite.svg#logo"></use>
+                </svg>
+            </a>
+        </div>
+        <nav class="navbar ms-auto">
+            <ul>
+                <li>
+                    <a class="nav-links active" aria-current="page" href="javascript:;">Design</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="javascript:;">Développement</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="javascript:;">Illustration</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="javascript:;">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
