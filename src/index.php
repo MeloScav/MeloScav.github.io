bb9f15bbff0ad6d3d3e66ff4875c9f4e84561b61
MeloScav <meloscavezzoni@gmail.com>
2022-03-23 18:47:28 +0100

    Restructuration with PHP

--- src/index.php
+++ src/index.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php $title = 'Accueil'; ?>
+<?php ob_start(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
-</head>
+<?php
+$test = "test 3";
+echo $test;
+?>
 
-<body>
-    <?php
-    $test = "test";
-    echo $test;
-    ?>
-    <script src="./index.js"></script>
-</body>
-
-</html>
+<?php $content = ob_get_clean(); ?>
+<?php require('./views/_template.php'); ?>
