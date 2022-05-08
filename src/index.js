4146ab3e3c3fcbfa075ac8799c3eb82ebb30e66c
MeloScav <meloscavezzoni@gmail.com>
2022-05-08 14:49:22 +0200

    Remove transition and nav open

--- src/index.js
+++ src/index.js
@@ -7,5 +7,26 @@ function ready(fn) {
 }
 
 ready(() => {
-    // Code
+    const removeTransitionMd = document.querySelectorAll(".remove-transition-md");
+    const removeTransitionMdClass = () => {
+        let windowWidth = window.innerWidth;
+        if (windowWidth < 992) {
+            removeTransitionMd.forEach(element => {
+                element.classList.remove("remove-transition-md");
+            });
+        } else {
+            removeTransitionMd.forEach(element => {
+                element.classList.add("remove-transition-md");
+            });
+        }
+    }
+
+    removeTransitionMdClass();
+
+    window.addEventListener('resize', removeTransitionMdClass);
+
+    const navbarToggler = document.querySelector(".header-nav .navbar-toggle-wrapper");
+    navbarToggler.addEventListener("click", function () {
+        document.querySelector("body").classList.toggle("nav-open")
+    })
 })
