c87c0f0890c91636fa53615828d43c3a3a8d90d2
MeloScav <meloscavezzoni@gmail.com>
2022-05-25 13:45:49 +0200

    Structure + content

---
+++ src/views/_see_more_project_section.php
@@ -0,0 +1,40 @@
+<section class="bg-grey-100">
+    <div class="container section-stack-sm">
+        <h2 class="text-center">Voir plus de projets</h2>
+        <div class="cards-container">
+            <div class="card card-design">
+                <div class="card-thumb">
+                    <svg>
+                        <use href="../assets/sprite.svg#design-icon"></use>
+                    </svg>
+                </div>
+                <div class="card-content text-lg">
+                    <p>Design</p>
+                </div>
+                <a href="javascript:;" class="stretched-link"></a>
+            </div>
+            <div class="card card-devlpment">
+                <div class="card-thumb">
+                    <svg>
+                        <use href="../assets/sprite.svg#dvlpmt-icon"></use>
+                    </svg>
+                </div>
+                <div class="card-content text-lg">
+                    <p>Développement</p>
+                </div>
+                <a href="javascript:;" class="stretched-link"></a>
+            </div>
+            <div class="card card-illu">
+                <div class="card-thumb">
+                    <svg>
+                        <use href="../assets/sprite.svg#illu-icon"></use>
+                    </svg>
+                </div>
+                <div class="card-content text-lg">
+                    <p>Illustration</p>
+                </div>
+                <a href="javascript:;" class="stretched-link"></a>
+            </div>
+        </div>
+    </div>
+</section>
