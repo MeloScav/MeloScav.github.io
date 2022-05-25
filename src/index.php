3759e63a500ac8b28a3dd0be0be23c911e50ea18
MeloScav <meloscavezzoni@gmail.com>
2022-05-25 13:48:49 +0200

    Include : banner, projects, see more project, about me

--- src/index.php
+++ src/index.php
@@ -2,9 +2,16 @@
 <?php ob_start(); ?>
 
 <?php
-$test = "test 3";
-echo $test;
+// $test = "<h1>test</h1><p>test</p>";
+// echo $test;
 ?>
+
+<?php include("./views/_banner.php"); ?>
+<div class="section-stack section-py">
+    <?php include("./views/_projects_section.php"); ?>
+    <?php include("./views/_see_more_project_section.php"); ?>
+    <?php include("./views/_about_me_section.php"); ?>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('./views/_template.php'); ?>
