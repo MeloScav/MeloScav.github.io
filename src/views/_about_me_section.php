9681f2c53d47eb280db4760c6af276f589acd3c4
MeloScav <meloscavezzoni@gmail.com>
2022-05-25 13:47:36 +0200

    Structure + content

---
+++ src/views/_about_me_section.php
@@ -0,0 +1,33 @@
+<section class="bg-white">
+    <div class="container section-stack-sm">
+        <h2>A propos</h2>
+        <div class="about-me">
+            <img class="about-me-img" src="./../assets/minimalist-illustration-woman.svg" alt="Minimalist illustration of a woman">
+            <div class="about-me-content">
+                <h3 class="about-me-content-title text-primary">Méloé</h3>
+                <div class="line"></div>
+                <div class="about-me-content-info">
+                    <div class="about-me-content-info-text">
+                        <p>
+                            Développeuse web frontend de base et passionnée par tout ce
+                            qui est visuel, j’ai décidé de me lancer dans l’apprentissage de l’UI / UX Design.
+                        </p>
+                        <p>
+                            Dans mon temps libre, j’aime dessiner, peindre, me promener ainsi que de découvrir des
+                            nouvelles technologies.
+                        </p>
+                        <p>
+                            <span>Autodidacte</span>,<span>curieuse</span> et <span>motivée</span>, j’ai envie
+                            d’<span>apprendre des nouvelles choses</span> et de
+                            <span>perfectionner mes connaissances</span>.
+                        </p>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center grid-gap gap-32">
+                        <a class="btn btn-outline-primary" href="javascript:;">Télécharger mon CV</a>
+                        <a class="btn btn-primary" href="javascript:;">Contactez-moi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
