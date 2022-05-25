1997f340ae10680fffd6c0c31e3337ac0b5dfb10
MeloScav <meloscavezzoni@gmail.com>
2022-05-25 12:35:17 +0200

    Structure + content

---
+++ src/views/_projects_section.php
@@ -0,0 +1,38 @@
+<section class="bg-light-secondary">
+    <div class="container section-stack-sm">
+        <h2>Mes projets récents</h2>
+        <div class="project project-left project-beer-lovers">
+            <div class="box-image">
+                <img src="./../assets/beer-lovers-festival.jpg" alt="Beer Lovers Festival">
+                <div class="overlay"></div>
+            </div>
+            <div class="project-info">
+                <h3 class="h4">Beer lovers’ Festival</h3>
+                <p>Création du design pour le Summer Beer Lovers’ Festival.</p>
+                <a href="javascript:;" class="btn btn-outline-primary">En savoir plus</a>
+            </div>
+        </div>
+        <div class="project project-right project-family-icons">
+            <div class="box-image">
+                <img src="./../assets/image-icons-family.jpg" alt="Family icons">
+                <div class="overlay"></div>
+            </div>
+            <div class="project-info">
+                <h3 class="h4">Ensemble d’icônes</h3>
+                <p>Création de 4 icônes de transport.</p>
+                <a href="javascript:;" class="btn btn-outline-primary">En savoir plus</a>
+            </div>
+        </div>
+        <div class="project project-left project-illu-car">
+            <div class="box-image">
+                <img src="./../assets/image-illustration-car.jpg" alt="Illustration of a car with gifts">
+                <div class="overlay"></div>
+            </div>
+            <div class="project-info">
+                <h3 class="h4">Illustration vectorielle</h3>
+                <p>Création d’une voiture dans un paysage d’été.</p>
+                <a href="javascript:;" class="btn btn-outline-primary">En savoir plus</a>
+            </div>
+        </div>
+    </div>
+</section>
