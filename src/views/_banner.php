b4b90e084ecdcb4e269c7425352ffea47a1c5733
MeloScav <meloscavezzoni@gmail.com>
2022-05-25 12:28:58 +0200

    Banner content + illustration and waves svg

---
+++ src/views/_banner.php
@@ -0,0 +1,44 @@
+<div class="container-full main-banner-wrapper">
+    <div class="container">
+        <!-- <div class="container-content"> -->
+        <div class="main-banner">
+            <div class="main-banner-content">
+
+                <p class="main-banner-sub-title text-md">Frontend web developer & UI / UX Designer</p>
+                <h1 class="main-banner-title">Méloé Scavezzoni</h1>
+
+                <p class="main-banner-description text-lg">
+                    Actuellement à la recherche d’un <span class="text-secondary fw-bold">job</span>
+                </p>
+                <div class="d-flex flex-wrap align-items-center grid-gap gap-32">
+                    <a class="btn btn-outline-primary" href="javascript:;">voir plus</a>
+                    <a class="btn btn-primary" href="javascript:;">Contactez-moi</a>
+                </div>
+                <div class="icons-container d-flex flex-wrap align-items-center grid-gap gap-32">
+                    <a href="javascript:;">
+                        <svg class="icon icon-lg">
+                            <use href="../assets/sprite.svg#github"></use>
+                        </svg>
+                    </a>
+                    <a href="javascript:;">
+                        <svg class="icon icon-lg">
+                            <use href="../assets/sprite.svg#linkedin"></use>
+                        </svg>
+                    </a>
+                </div>
+
+            </div>
+            <img class="main-banner-illu" src="../assets/me-cartoon-illustration.svg" />
+        </div>
+    </div>
+
+    <div class="main-banner-waves-wrapper">
+        <svg class="main-banner-wave wave-1" viewBox="0 0 1920 73" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#DCE8FB" d="M1920.41 33.1643C1689.5 -18.6592 1197.24 47.0527 969.624 47.1193C742.012 47.186 173.946 -48.2253 0.0305806 33.7268C0.0404059 67.2706 0.0209177 72.0017 0.0209177 72.0017L969.631 71.8705L1920.4 71.7419C1920.4 71.7419 1920.4 50.1109 1920.41 33.1643Z" />
+        </svg>
+        <svg class="main-banner-wave wave-2" viewBox="0 0 1920 92" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 53.764C338 62.1013 645 0.57891 962 0.00386357C1279 -0.571183 1412 63.2512 1920 53.764V92H0V53.764Z" fill="#DCE8FB" fill-opacity="0.51" />
+        </svg>
+    </div>
+
+</div>
