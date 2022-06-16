06ef9ee1b2b69098656fd933145638852e6b9f95
MeloScav <meloscavezzoni@gmail.com>
2022-06-16 11:24:29 +0200

    Add projects informations

---
+++ src/data/database.php
@@ -0,0 +1,144 @@
+<?php
+global $PROJECTS;
+$PROJECTS = [
+    0 => [
+        "title" => "Pomodoro",
+        "type" => [
+            0 => "development"
+        ],
+        "cssClass" => "project-pomodoro",
+        "shortDescription" => "",
+        "link" => "/projects/pomodoro/details.php",
+        "introductionBanner" => "/assets/react-pomodoro.jpg",
+        "slideBanner" => "/assets/react-pomodoro.jpg",
+        "detailsBanner" => "/assets/beer-lover-banner.jpg"
+    ],
+    1 => [
+        "title" => "Beer lovers’ Festival",
+        "type" =>  [
+            0 => "design"
+        ],
+        "cssClass" => "project-beer-lovers",
+        "shortDescription" => "Création du design d’un site pour le Summer Beer Lovers’ Festival.",
+        "link" => "/projects/beer-lovers/details.php",
+        "introductionBanner" => "/assets/beer-lovers-festival.jpg",
+        "slideBanner" => "/assets/beer-lovers-festival.jpg",
+        "detailsBanner" => "/assets/beer-lover-banner.jpg",
+        "presentation" => [
+            "text" => [
+                0 => "Dans le cadre d’un cours, dans lequel on devait réaliser un design, j’ai choisi de prendre les festivitées du “Summer Beer Lovers’ Festival” qui est un festival qui a lieu chaque été au centre de la ville de Liège.",
+                1 => "Ce festival met en avant les brasseries artisanales wallones, avec environ 20 à 30 brasseries participants chaque année et plus de 100 bières spéciales à y découvrir.",
+                2 => "J’ai redesigner un site pour cet évènement en y transmettant l’esprit du festival. Un style festif, jeune, quelque chose faisant penser à l’été et qui donne envie d’aller s’amuser, tout en mettant la bière en avant."
+            ],
+            "buttonLink" => "",
+            "image" => "/assets/beer-lover-festival-presentation.jpg"
+        ],
+        "detailsColors" => [
+            "colors" => [
+                0 => [
+                    "name" => "yellow",
+                    "colorHex" => "#FCD673",
+                    "text" => "J’ai choisi d’utiliser une nuance de jaune en couleur principal, pour faire référence à la bière mais aussi parce que c’est une nuance qui reste clair et estival, elle permet également de donner un sentiment de joie aux visiteurs."
+                ],
+                1 => [
+                    "name" => "blue",
+                    "colorHex" => "#01cee2",
+                    "text" => "En couleur secondaire, j’ai choisi un bleu clair éclatant pour donner un sentiment de peps, festif, tout en gardant un côté permettant de s’y sentir bien."
+                ],
+                2 => [
+                    "name" => "red",
+                    "colorHex" => "#ffb198",
+                    "text" => "En troisième couleur, j’ai décidé d’ajouter une petite pointe de rouge-orangé. Il permet de renforcer le côté fête, été, vacance, et un peu plus chaleureux."
+                ],
+                3 => [
+                    "name" => "black",
+                    "colorHex" => "#171612",
+                    "text" => "Pour le texte, j’ai choisi un noir légérement saturé avec la couleur principale."
+                ],
+                4 => [
+                    "name" => "white",
+                    "colorHex" => "#fffdf6",
+                    "text" => "Le blanc utilisé est également saturé avec un peu de jaune."
+                ]
+            ],
+            "image" => "/assets/macbook-beer-lovers-festival.png"
+        ],
+        "fonts" => [
+            0 => [
+                "image" => "/assets/details-font-title-beer-lovers.svg",
+                "title" => "Les titres",
+                "text" => [
+                    0 => "Pour les titres, j’ai choisi d’utiliser la police Lobter.",
+                    1 => "Italique et effet manuscrit, elle permet de donner un effet personnel au site."
+                ]
+            ],
+            1 => [
+                "image" => "/assets/details-font-text-beer-lovers.svg",
+                "title" => "Les textes",
+                "text" => [
+                    0 => "Pour le texte, j’ai utilisé la police Quicksand.",
+                    1 => "C’est une police sans empatement qui permet de donner un effet intemporel."
+                ]
+            ]
+        ],
+        "icons" => [
+            0 => [
+                "icon" => "/assets/icon-beer.svg",
+                "iconAlt" => "Icon beer",
+                "text" => [
+                    0 => "J’ai créé une icône de bière pour mettre en grand au niveau du banner principal ainsi que dans le footer.",
+                    1 => "Il permet de mettre directement l’accent sur la bière, qui est l’élément principal du festival, de montrer au futur participant qu’ils auront de quoi boire et s’amuser, ainsi que de rajouter un côté plus visuel au site."
+                ],
+                "image" => "/assets/macbook-beer-lovers-festival-2.png"
+            ]
+        ],
+        "design" => [
+            0 => [
+                "text" => [
+                    0 => "Mon public cible, ce sont les participants de festivals ainsi que les passionnés de bières.",
+                    1 => "Mon objectif est de retenir l’attention du visiteur ainsi que de l’amener à s’inscrire pour l’évènement.",
+                    2 => "Pour ce faire, je suis restée sur quelque chose d’assez fluide, avec des formes et des vagues pour donner un effet jeune, moderne, et de renforcer encore plus le côté festif. J’ai placé un bouton d’inscription directement visible sur le banner, en ouvrant le site, il y a également une section d’informations sur le festival, une section pour pour donner envie aux visiteurs de participer avec un bouton pour s’inscrire et une Google Map pour visualiser le lieu précis de ces festivités.",
+                    3 => "J’ai aussi créé une icône de bière qui permet de mettre l’accent sur la bière qui est un élément essentiel du festival. Elle donne un côté plus visuel et festif au site."
+                ],
+                "image" => "/assets/beer-lovers-festival-home.jpg"
+            ],
+            1 => [
+                "text" => [
+                    0 => "J’ai construit la page pour que l’oeil du visiteur arrive d’abord sur le milieu du banner, où il y a le titre et le bouton d’action puis ensuite sur la bière et en dernier, sur la barre de navigation. Le visiteur a directement les informations importantes en ouvrant le site et il peut aisément s’inscrire à la participation du festival.",
+                    1 => "Ensuite, il est dirigé vers une section contenant des parties d’informations, un peu plus poussées, sur le festival ainsi que des boutons d’actions pour en savoir plus s’il le désire.",
+                    2 => "Il verra ensuite une section servant à donner envie au visiteur de participer à la fête. Elle est composée d’un texte accrocheur et d’un bouton d’inscription.",
+                    3 => "Après cela, son oeil ira vers une Google Map, utile pour visualiser correctement le lieu des festivités, puis vers une phrase d’accroche invitant la personne a venir.",
+                    4 => "En dernier lieu, il verra, dans le bas de page, l’icône de bière. Celle-ci permet de rappeler la fête, la bière, et faire référence à la phrase d’accroche juste au-dessus invitant le visiteur à venir boire un verre. Son regard se dirigera, après cela, sur les différents liens du site, présent juste en-dessous, ainsi que sur les différents moyens de contact."
+                ],
+                "image" => "/assets/design-beer-lovers-explain.jpg"
+            ]
+        ]
+    ],
+    2 => [
+        "title" => "Ensemble d’icônes",
+        "type" =>  [
+            0 => "illustration"
+        ],
+        "cssClass" => "project-family-icons",
+        "shortDescription" => "Création de 4 icônes de transport.",
+        "link" => "/projects/family-icons/details.php",
+        "introductionBanner" => "/assets/image-icons-family.jpg",
+        "slideBanner" => "/assets/image-icons-family.jpg",
+        "detailsBanner" => "/assets/beer-lover-banner.jpg"
+    ],
+    3 => [
+        "title" => "Illustration vectorielle",
+        "type" => [
+            0 => "illustration"
+        ],
+        "cssClass" => "project-illu-car",
+        "shortDescription" => "Création d’une voiture dans un paysage d’été.",
+        "link" => "/projects/illu-car/details.php",
+        "introductionBanner" => "/assets/image-illustration-car.jpg",
+        "slideBanner" => "/assets/image-illustration-car.jpg",
+        "detailsBanner" => "/assets/beer-lover-banner.jpg"
+    ],
+];
+?>
+
+<?php ?>
