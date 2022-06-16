24025bf9cb5946f5705ab803bd2a98e7e0d0bd08
MeloScav <meloscavezzoni@gmail.com>
2022-06-16 11:37:31 +0200

    Structure + text

---
+++ src/views/_call_to_action_section.php
@@ -0,0 +1,5 @@
+<section class="call-to-action-section">
+    <div class="container">
+        <h3>Ça vous a plu ?<br>N’hésitez pas à me <a href="mailto:meloscavezzoni@gmail.com">contacter</a> !</h3>
+    </div>
+</section>
