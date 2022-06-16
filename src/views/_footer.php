04f3b3e7f348f755380cd5e7ae65d8a5a13eed13
MeloScav <meloscavezzoni@gmail.com>
2022-06-16 11:37:45 +0200

    Structure + text

---
+++ src/views/_footer.php
@@ -0,0 +1,49 @@
+<footer class="footer">
+    <div class="footer-wave-wrapper">
+        <svg class="footer-wave" data-animate-wave viewBox="0 0 1920 127" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M0 75.4211C138.99 25.642 278.481 -41.5072 828.5 33.7086C1378.52 108.924 1693 103 1920 108.924C1920 121.5 1920 127.999 1920 127.999H0C0 127.999 0 84.4584 0 75.4211Z"
+                fill="#F2DFF6" />
+        </svg>
+    </div>
+    <div class="footer-content bg-light-primary">
+        <div class="container">
+            <a href="/">
+                <svg class="footer-logo">
+                    <use href="/assets/sprite.svg#logo-footer"></use>
+                </svg>
+            </a>
+            <div class="footer-content-links">
+                <div class="footer-content-links-menu">
+                    <h4 class="footer-title">Menu</h4>
+                    <ul class="list-unstyled">
+                        <li><a class="footer-link" href="design.php">Design</a></li>
+                        <li><a class="footer-link" href="development.php">Développement</a></li>
+                        <li><a class="footer-link" href="illustration.php">Illustration</a></li>
+                    </ul>
+                </div>
+                <div class="footer-content-links-contact">
+                    <h4 class="footer-title">Contact</h4>
+                    <a class="footer-link" href="mailto:meloscavezzoni@gmail.com">meloscavezzoni@gmail.com</a>
+                </div>
+                <div class="footer-content-links-social-network">
+                    <h4 class="footer-title">Retrouvez-moi sur</h4>
+                    <div class="icons-container d-flex flex-wrap align-items-center grid-gap gap-32">
+                        <a href="https://github.com/MeloScav">
+                            <svg class="icon icon-md">
+                                <use href="/assets/sprite.svg#github-black"></use>
+                            </svg>
+                        </a>
+                        <a href="https://www.linkedin.com/in/meloe-scavezzoni/">
+                            <svg class="icon icon-md">
+                                <use href="/assets/sprite.svg#linkedin-black"></use>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
