644a7de8511f9d19cf398606df1186d3286d4d64
MeloScav <meloscavezzoni@gmail.com>
2022-06-16 11:36:43 +0200

    Image + text + email

--- src/views/_about_me_section.php
+++ src/views/_about_me_section.php
@@ -2,7 +2,8 @@
     <div class="container section-stack-sm">
         <h2>A propos</h2>
         <div class="about-me">
-            <img class="about-me-img" src="./../assets/minimalist-illustration-woman.svg" alt="Minimalist illustration of a woman">
+            <img class="about-me-img" src="./../assets/minimalist-illustration-woman.svg"
+                alt="Minimalist illustration of a woman">
             <div class="about-me-content">
                 <h3 class="about-me-content-title text-primary">Méloé</h3>
                 <div class="line"></div>
@@ -17,14 +18,29 @@
                             nouvelles technologies.
                         </p>
                         <p>
-                            <span>Autodidacte</span>,<span>curieuse</span> et <span>motivée</span>, j’ai envie
-                            d’<span>apprendre des nouvelles choses</span> et de
-                            <span>perfectionner mes connaissances</span>.
+                            <strong class="text-secondary">
+                                Autodidacte
+                            </strong>,
+                            <strong class="text-secondary">
+                                curieuse
+                            </strong>
+                            et
+                            <strong class="text-secondary">
+                                motivée
+                            </strong>
+                            , j’ai envie d’
+                            <strong>
+                                apprendre des nouvelles choses
+                            </strong>
+                            et de
+                            <strong>
+                                perfectionner mes connaissances
+                            </strong>.
                         </p>
                     </div>
                     <div class="d-flex flex-wrap align-items-center grid-gap gap-32">
                         <a class="btn btn-outline-primary" href="javascript:;">Télécharger mon CV</a>
-                        <a class="btn btn-primary" href="javascript:;">Contactez-moi</a>
+                        <a class="btn btn-primary" href="mailto:meloscavezzoni@gmail.com">Contactez-moi</a>
                     </div>
                 </div>
             </div>
