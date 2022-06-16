04ea7d147da882993476c004774381b30f1e5b50
MeloScav <meloscavezzoni@gmail.com>
2022-06-16 11:59:39 +0200

    Add contact button

--- src/views/_call_to_action_section.php
+++ src/views/_call_to_action_section.php
@@ -1,5 +1,6 @@
 <section class="call-to-action-section">
     <div class="container">
-        <h3>Ça vous a plu ?<br>N’hésitez pas à me <a href="mailto:meloscavezzoni@gmail.com">contacter</a> !</h3>
+        <h3>Ça vous a plu ?<br>N’hésitez pas à me contacter !</h3>
+        <a class="btn btn-primary" href="mailto:meloscavezzoni@gmail.com">Contactez-moi</a>
     </div>
 </section>
