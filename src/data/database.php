a8da4bc75e73d4d3025b2bf0a320900ff8d7c0bb
MeloScav <meloscavezzoni@gmail.com>
2022-06-21 16:15:54 +0200

    Add projects

--- src/data/database.php
+++ src/data/database.php
@@ -2,18 +2,143 @@
 global $PROJECTS;
 $PROJECTS = [
     0 => [
+        "title" => "Mon ancien site web",
+        "type" => [
+            0 => "development"
+        ],
+        "cssClass" => "project-old-website",
+        "shortDescription" => "Mon ancien et premier site portfolio.",
+        "link" => "/projects/old-website/details.php",
+        "introductionBanner" => "/assets/old-website/my-old-website-first.jpeg",
+        "slideBanner" => "/assets/old-website/my-old-website-first.jpeg",
+        "detailsBanner" => "/assets/old-website/bg-banner-old-website.jpg",
+        "presentation" => [
+            "text" => [
+                0 => "Mon premier site portfolio que j'ai réalisé en sortant de ma formation de développeuse web frontend.",
+                1 => "Je l'ai réalisé en utilisant la bibliothèque javascript REACT JS.",
+            ],
+            "buttonLink" => "https://meloescavezzoni.netlify.app/",
+            "image" => "/assets/old-website/my-old-website-choice.jpeg"
+        ],
+        "detailsColors" => [
+            "cssClass" => "old-website",
+            "colors" => [
+                0 => [
+                    "name" => "blue",
+                    "colorHex" => "#1497d4",
+                    "text" => "En couleur primaire, j'ai utilisé un bleu plutôt clair. Cette couleur reflète le sérieux et la confiance."
+                ],
+                1 => [
+                    "name" => "dark",
+                    "colorHex" => "#000000",
+                    "text" => "En couleur secondaire, j'ai utilisé un noir pour permettre de faire ressortir la couleur primaire et de donner un côté sophistiqué au site."
+                ]
+            ],
+            "image" => "/assets/old-website/macbook-old-website.png"
+        ]
+    ],
+    1 => [
+        "title" => "Mon ancien site portfolio",
+        "type" => [
+            0 => "development"
+        ],
+        "cssClass" => "project-old-site-portfolio",
+        "shortDescription" => "Réalisation de mon site portfolio en One Page.",
+        "link" => "/projects/old-site/details.php",
+        "introductionBanner" => "/assets/old-portfolio/old-website-view-home.jpg",
+        "slideBanner" => "/assets/old-portfolio/old-website-img-slider.jpg",
+        "detailsBanner" => "/assets/old-portfolio/bg-banner-old-website-portfolio.jpg",
+        "presentation" => [
+            "text" => [
+                0 => "J'ai créé ce site car je trouvais que mon ancien site portfolio était devenu trop vieux. Je l'ai donc redesigner en un site plus simple et plus représentatif de mon image et personnalité.",
+                1 => "Ce site est un One Page réalisé en utilisant uniquement du HTML, SCSS et JavaScript."
+            ],
+            "buttonLink" => "https://meloscav.netlify.app/",
+            "image" => "/assets/old-portfolio/old-website-presentation.jpeg"
+        ],
+        "detailsColors" => [
+            "cssClass" => "old-portfolio",
+            "colors" => [
+                0 => [
+                    "name" => "purple",
+                    "colorHex" => "#c74db2",
+                    "text" => "En couleur primaire, j'ai utilisé un mauve clair qui permet de donner un côté créatif et féminin au site."
+                ],
+                1 => [
+                    "name" => "dark-text",
+                    "colorHex" => "#141414",
+                    "text" => "Pour le texte, j'ai utilisé une nuance de noir."
+                ],
+                2 => [
+                    "name" => "white",
+                    "colorHex" => "#f8f8f8",
+                    "text" => "Pour une autre partie du texte, j'ai utilisé une nuance de blanc."
+                ]
+            ],
+            "image" => "/assets/old-portfolio/macbook-old-webiste-portfolio.png"
+        ],
+        "fonts" => [
+            0 => [
+                "image" => "/assets/old-portfolio/details-font-old-portfolio.svg",
+                "title" => "Les titres et textes",
+                "text" => [
+                    0 => "J'ai utilisé la police Montserrat pour l'ensemble du site.",
+                ]
+            ]
+        ]
+    ],
+    2 => [
         "title" => "Pomodoro",
         "type" => [
             0 => "development"
         ],
         "cssClass" => "project-pomodoro",
-        "shortDescription" => "",
+        "shortDescription" => "Réalisation d'un pomodoro en REACT permettant de gérer son temps de travail et de pause.",
         "link" => "/projects/pomodoro/details.php",
-        "introductionBanner" => "/assets/react-pomodoro.jpg",
-        "slideBanner" => "/assets/react-pomodoro.jpg",
-        "detailsBanner" => "/assets/beer-lover-banner.jpg"
+        "introductionBanner" => "/assets/pomodoro/react-pomodoro.jpeg",
+        "slideBanner" => "/assets/pomodoro/react-pomodoro.jpeg",
+        "detailsBanner" => "/assets/pomodoro/bg-banner-pomodoro.jpg",
+        "presentation" => [
+            "text" => [
+                0 => "Dans le cardre de ma formation en tant que Frontend Web Developer chez Becode, j'ai réalisé un Pomodoro avec pour principal objectif d'en apprendre plus sur REACT et ses hooks.",
+                1 => "Grâce à ce Pomodoro, vous pouvez enfin gérer facilement vos temps de travail et de pause. Il suffit juste d'y entrer la durée de travail désirée puis, quand vous y serez invité, la durée de la pause souhaitée."
+            ],
+            "buttonLink" => "https://pomodoro-ms.netlify.app/",
+            "image" => "/assets/pomodoro/react-pomodoro-modal.jpeg"
+        ],
+        "detailsColors" => [
+            "cssClass" => "pomodoro",
+            "colors" => [
+                0 => [
+                    "name" => "dark-rose",
+                    "colorHex" => "#ac7b7d",
+                    "text" => "En couleur primaire, j'ai décidé d'utiliser une couleur rose foncé tirant sur le rouge."
+                ],
+                1 => [
+                    "name" => "dark-blue",
+                    "colorHex" => "#34435e",
+                    "text" => "En couleur secondaire, j’ai choisi un bleu foncé qui permet notamment de faire resortir la couleur primaire. Il donne également un côté sérieux et calme."
+                ],
+                2 => [
+                    "name" => "pure-white",
+                    "colorHex" => "#ffffff",
+                    "text" => "Pour le texte, j'ai utilisé du blanc."
+                ]
+            ],
+            "image" => "/assets/pomodoro/macbook-pomodoro.png"
+        ],
+        "fonts" => [
+            0 => [
+                "image" => "/assets/pomodoro/details-font-pomodoro.svg",
+                "title" => "Les titres et textes",
+                "text" => [
+                    0 => "J'ai utilisé la police Arial pour l'ensemble du pomodoro.",
+                    1 => "Elle donne un côté assez neutre et minimaliste au site."
+                ]
+            ]
+        ]
     ],
-    1 => [
+    3 => [
         "title" => "Beer lovers’ Festival",
         "type" =>  [
             0 => "design"
@@ -21,9 +146,9 @@ $PROJECTS = [
         "cssClass" => "project-beer-lovers",
         "shortDescription" => "Création du design d’un site pour le Summer Beer Lovers’ Festival.",
         "link" => "/projects/beer-lovers/details.php",
-        "introductionBanner" => "/assets/beer-lovers-festival.jpg",
-        "slideBanner" => "/assets/beer-lovers-festival.jpg",
-        "detailsBanner" => "/assets/beer-lover-banner.jpg",
+        "introductionBanner" => "/assets/beer-lovers/beer-lovers-festival.jpg",
+        "slideBanner" => "/assets/beer-lovers/beer-lovers-festival.jpg",
+        "detailsBanner" => "/assets/beer-lovers/beer-lover-banner.jpg",
         "presentation" => [
             "text" => [
                 0 => "Dans le cadre d’un cours, dans lequel on devait réaliser un design, j’ai choisi de prendre les festivitées du “Summer Beer Lovers’ Festival” qui est un festival qui a lieu chaque été au centre de la ville de Liège.",
@@ -31,13 +156,14 @@ $PROJECTS = [
                 2 => "J’ai redesigner un site pour cet évènement en y transmettant l’esprit du festival. Un style festif, jeune, quelque chose faisant penser à l’été et qui donne envie d’aller s’amuser, tout en mettant la bière en avant."
             ],
             "buttonLink" => "",
-            "image" => "/assets/beer-lover-festival-presentation.jpg"
+            "image" => "/assets/beer-lovers/beer-lover-festival-presentation.jpg"
         ],
         "detailsColors" => [
+            "cssClass" => "beer-lovers",
             "colors" => [
                 0 => [
                     "name" => "yellow",
-                    "colorHex" => "#FCD673",
+                    "colorHex" => "#fcd673",
                     "text" => "J’ai choisi d’utiliser une nuance de jaune en couleur principal, pour faire référence à la bière mais aussi parce que c’est une nuance qui reste clair et estival, elle permet également de donner un sentiment de joie aux visiteurs."
                 ],
                 1 => [
@@ -61,11 +187,11 @@ $PROJECTS = [
                     "text" => "Le blanc utilisé est également saturé avec un peu de jaune."
                 ]
             ],
-            "image" => "/assets/macbook-beer-lovers-festival.png"
+            "image" => "/assets/beer-lovers/macbook-beer-lovers-festival.png"
         ],
         "fonts" => [
             0 => [
-                "image" => "/assets/details-font-title-beer-lovers.svg",
+                "image" => "/assets/beer-lovers/details-font-title-beer-lovers.svg",
                 "title" => "Les titres",
                 "text" => [
                     0 => "Pour les titres, j’ai choisi d’utiliser la police Lobter.",
@@ -73,7 +199,7 @@ $PROJECTS = [
                 ]
             ],
             1 => [
-                "image" => "/assets/details-font-text-beer-lovers.svg",
+                "image" => "/assets/beer-lovers/details-font-text-beer-lovers.svg",
                 "title" => "Les textes",
                 "text" => [
                     0 => "Pour le texte, j’ai utilisé la police Quicksand.",
@@ -83,13 +209,13 @@ $PROJECTS = [
         ],
         "icons" => [
             0 => [
-                "icon" => "/assets/icon-beer.svg",
+                "icon" => "/assets/beer-lovers/icon-beer.svg",
                 "iconAlt" => "Icon beer",
                 "text" => [
                     0 => "J’ai créé une icône de bière pour mettre en grand au niveau du banner principal ainsi que dans le footer.",
                     1 => "Il permet de mettre directement l’accent sur la bière, qui est l’élément principal du festival, de montrer au futur participant qu’ils auront de quoi boire et s’amuser, ainsi que de rajouter un côté plus visuel au site."
                 ],
-                "image" => "/assets/macbook-beer-lovers-festival-2.png"
+                "image" => "/assets/beer-lovers/macbook-beer-lovers-festival-2.png"
             ]
         ],
         "design" => [
@@ -100,7 +226,7 @@ $PROJECTS = [
                     2 => "Pour ce faire, je suis restée sur quelque chose d’assez fluide, avec des formes et des vagues pour donner un effet jeune, moderne, et de renforcer encore plus le côté festif. J’ai placé un bouton d’inscription directement visible sur le banner, en ouvrant le site, il y a également une section d’informations sur le festival, une section pour pour donner envie aux visiteurs de participer avec un bouton pour s’inscrire et une Google Map pour visualiser le lieu précis de ces festivités.",
                     3 => "J’ai aussi créé une icône de bière qui permet de mettre l’accent sur la bière qui est un élément essentiel du festival. Elle donne un côté plus visuel et festif au site."
                 ],
-                "image" => "/assets/beer-lovers-festival-home.jpg"
+                "image" => "/assets/beer-lovers/beer-lovers-festival-home.jpg"
             ],
             1 => [
                 "text" => [
@@ -110,11 +236,11 @@ $PROJECTS = [
                     3 => "Après cela, son oeil ira vers une Google Map, utile pour visualiser correctement le lieu des festivités, puis vers une phrase d’accroche invitant la personne a venir.",
                     4 => "En dernier lieu, il verra, dans le bas de page, l’icône de bière. Celle-ci permet de rappeler la fête, la bière, et faire référence à la phrase d’accroche juste au-dessus invitant le visiteur à venir boire un verre. Son regard se dirigera, après cela, sur les différents liens du site, présent juste en-dessous, ainsi que sur les différents moyens de contact."
                 ],
-                "image" => "/assets/design-beer-lovers-explain.jpg"
+                "image" => "/assets/beer-lovers/design-beer-lovers-explain.jpg"
             ]
         ]
     ],
-    2 => [
+    4 => [
         "title" => "Ensemble d’icônes",
         "type" =>  [
             0 => "illustration"
@@ -122,11 +248,31 @@ $PROJECTS = [
         "cssClass" => "project-family-icons",
         "shortDescription" => "Création de 4 icônes de transport.",
         "link" => "/projects/family-icons/details.php",
-        "introductionBanner" => "/assets/image-icons-family.jpg",
-        "slideBanner" => "/assets/image-icons-family.jpg",
-        "detailsBanner" => "/assets/beer-lover-banner.jpg"
+        "introductionBanner" => "/assets/family-icons/image-icons-family.jpg",
+        "slideBanner" => "/assets/family-icons/image-icons-family.jpg",
+        "detailsBanner" => "/assets/family-icons/bg-banner-family-icons.jpg",
+        "presentation" => [
+            "text" => [
+                0 => "Dans le cadre d’un cours d'introduction à la création d'icônes sur Figma, dans lequel on devait réaliser un ensemble d'icônes, j'ai décidé de prendre les transports comme sujet.",
+                1 => "J'ai donc réalisé un ensemble d'icônes représentant divers moyens de transport : la voiture, le bus, le train ainsi que la trottinnette.",
+                2 => "Pour cet exercice, j'ai décidé de rester sur un design assez simple et neutre."
+            ],
+            "buttonLink" => "",
+            "image" => "/assets/family-icons/icons-family.jpg"
+        ],
+        "icons" => [
+            0 => [
+                "icon" => "/assets/family-icons-set.svg",
+                "iconAlt" => "Ensemble d'icones représentant des moyens de transport",
+                "text" => [
+                    0 => "Les 4 icônes représentent des moyens de transports courant : la voiture, le bus, le train et la trottinette.",
+                    1 => "Elles sont toutes composées de formes géométriques avec un style simple. "
+                ],
+                "image" => "/assets/family-icons/macbook-family-icons.png"
+            ]
+        ]
     ],
-    3 => [
+    5 => [
         "title" => "Illustration vectorielle",
         "type" => [
             0 => "illustration"
@@ -134,9 +280,18 @@ $PROJECTS = [
         "cssClass" => "project-illu-car",
         "shortDescription" => "Création d’une voiture dans un paysage d’été.",
         "link" => "/projects/illu-car/details.php",
-        "introductionBanner" => "/assets/image-illustration-car.jpg",
-        "slideBanner" => "/assets/image-illustration-car.jpg",
-        "detailsBanner" => "/assets/beer-lover-banner.jpg"
+        "introductionBanner" => "/assets/illu-car/illu-car.jpg",
+        "slideBanner" => "/assets/illu-car/illu-car.jpg",
+        "detailsBanner" => "/assets/illu-car/bg-banner-illu-car.jpg",
+        "presentation" => [
+            "text" => [
+                0 => "Dans le cadre d’un cours sur Illustrator, j'ai dû créer une illustration avec comme contrainte une voiture et sa remorque.",
+                1 => "Comme à ce moment, on était dans la période des fêtes de fin d'année, j'ai décide de faire une voiture avec sa remorque remplie de cadeaux, le tout dans un paysage paisible et ensoleillé.",
+                2 => ""
+            ],
+            "buttonLink" => "",
+            "image" => "/assets/illu-car/illu-car.jpg"
+        ]
     ],
 ];
 ?>
