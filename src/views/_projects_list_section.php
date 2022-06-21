8c0695993baf5e290a5df319edb9537380884570
MeloScav <meloscavezzoni@gmail.com>
2022-06-21 16:19:08 +0200

    Add array

--- src/views/_projects_list_section.php
+++ src/views/_projects_list_section.php
@@ -9,7 +9,7 @@
             if (isset($project['type']) && count($project['type']) > 0) {
                 foreach ($project['type'] as $projectType) {
                     if ($projectType == $projectsType) {
-                        $projectsListByType[] = $project;
+                        array_unshift($projectsListByType, $project);
                     }
                 }
             }
@@ -56,13 +56,15 @@
     </div>
 
     <div class="project-banner-waves-wrapper">
-        <svg class="project-banner-wave wave-1" viewBox="0 0 1920 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="project-banner-wave wave-1" data-animate-wave viewBox="0 0 1920 89" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M1920 0H0V59.5767C489.313 -4.69498 668.897 13.6333 943.013 41.6094C1127.89 60.4784 1355.78 83.7363 1750.7 89H1920V0Z"
                 fill="#F2DFF6" />
         </svg>
 
-        <svg class="project-banner-wave wave-2" viewBox="0 0 1920 103" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="project-banner-wave wave-2" data-animate-wave viewBox="0 0 1920 103" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M-1 0H1922V68.9487C1431.92 -5.43325 1252.06 15.7781 977.513 48.155C792.346 69.9918 564.109 96.9077 168.581 103H-1V0Z"
                 fill="#FBE9FF" fill-opacity="0.49" />
