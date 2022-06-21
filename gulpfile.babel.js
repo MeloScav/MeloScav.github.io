019a926f9b84b2f5e84a1cc3ef9aade228ac7400
MeloScav <meloscavezzoni@gmail.com>
2022-06-21 16:59:54 +0200

    Gulp seperate dev from build tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,15 +19,6 @@ const php2html = require("gulp-php2html");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass")(require("sass"));
 
-browserSync.init({
-    open: false,
-    port: 1234,
-    notify: false,
-    server: {
-        baseDir: "dist",
-    },
-});
-
 task("compile-php", (done) => {
     src(["./src/**/*.php", "!./src/**/_*.php"])
         .pipe(php2html())
@@ -57,7 +48,25 @@ task("copy-static-files", (done) => {
     done();
 });
 
-task("compile-sass", (done) => {
+task("compile-sass-dev", (done) => {
+    src(["./src/scss/**/*.scss", "./src/sass/**/*.scss", "!./src/scss/**/_*"])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                outputStyle: "compressed",
+                precision: 10,
+                includePaths: ["."],
+                onError: console.error.bind(console, "Sass error:"),
+            })
+        )
+        .pipe(sourcemaps.write("."))
+        .pipe(dest("./dist/css/"))
+        .pipe(browserSync.stream());
+    done();
+});
+
+task("compile-sass-build", (done) => {
     src(["./src/scss/**/*.scss", "./src/sass/**/*.scss", "!./src/scss/**/_*"])
         .pipe(plumber())
         .pipe(sourcemaps.init())
@@ -72,7 +81,6 @@ task("compile-sass", (done) => {
         .pipe(autoprefixer())
         .pipe(sourcemaps.write("."))
         .pipe(dest("./dist/css/"))
-        .pipe(browserSync.stream());
     done();
 });
 
@@ -95,14 +103,23 @@ const buildSeries = series(
     "copy-static-files",
     "compile-php",
     "pretty-html",
-    "compile-sass",
+    "compile-sass-build",
     "minify-js"
 );
 
 const devWatch = () => {
+    browserSync.init({
+        open: false,
+        port: 1234,
+        notify: false,
+        server: {
+            baseDir: "dist",
+        },
+    });
+
     watch("./src/**/*", task("copy-static-files"));
     watch("./src/**/*.php", series("compile-php", "pretty-html"));
-    watch("./src/scss/**/*", task("compile-sass"));
+    watch("./src/scss/**/*", task("compile-sass-dev"));
     watch("./src/**/*.js", task("minify-js"));
     watch(["./src/**/*", "!./src/**/*.s(a|c)ss"], task("reload-browser"));
 }
@@ -111,7 +128,7 @@ const dev = series(
     parallel("copy-static-files",
         "compile-php",
         "pretty-html",
-        "compile-sass",
+        "compile-sass-dev",
         "minify-js"),
     devWatch
 );
