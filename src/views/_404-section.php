364b11ec1583e91cc539481e8047875d1e2b3fdb
MeloScav <meloscavezzoni@gmail.com>
2022-08-05 12:23:14 +0200

    Add illustration + content + waves

---
+++ src/views/_404-section.php
@@ -0,0 +1,42 @@
+<section class="section-404 pt-main-nav-height">
+    <div class="container">
+        <div class="section-404-content">
+            <img class="img-400-illustration" src="../assets/404-page/404.svg" alt="Illustration 404 avec une pieuvre">
+            <div class="text-wrapper">
+                <p>
+                    Oh, vous vous êtes apparemment perdu dans l’océan !
+                </p>
+                <p>
+                    N’hésitez pas à retourner sur la page d’accueil ou à aller voir des magnifiques pieuvres dans cet
+                    océan.
+                </p>
+            </div>
+            <div class="buttons-wrapper d-flex flex-wrap grid-gap gap-32">
+                <a class="btn btn-outline-primary" href="/">
+                    Accueil
+                </a>
+                <a class="btn btn-primary" href="https://opensea.io/MeloScav?tab=created" rel="noopener noreferrer"
+                    target="_blank">
+                    Pieuvres
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="waves-wrapper-404">
+        <svg class="wave-404 wave-1" data-animate-wave viewBox="0 0 1920 243" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M0 51C328 -108 691.5 161 957 161C1216.5 161 1465.5 -105 1920 50C1920 152 1920 243 1920 243H0C0 243 1.28746e-05 167.5 0 51Z"
+                fill="#DCE8FB" />
+        </svg>
+
+        <svg class="wave-404 wave-2" data-animate-wave viewBox="0 0 1920 265" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M0 73C397.5 232.5 637 0.5 955 0.5C1257.5 0.5 1431 248.5 1920 72C1920 174 1920 265 1920 265H0C0 265 1.28746e-05 189.5 0 73Z"
+                fill="#DCE8FB" fill-opacity="0.5" />
+        </svg>
+
+    </div>
+</section>
