60fd4b37f8101556a6afa9695be05919242a0869
MeloScav <meloscavezzoni@gmail.com>
2022-08-11 11:30:08 +0200

    Prevent dark mode default behavior

--- src/views/_template.php
+++ src/views/_template.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="light only">
 
     <meta name="apple-mobile-web-app-title" content="M&eacute;lo&eacute; Scavezzoni">
     <meta name="application-name" content="M&eacute;lo&eacute; Scavezzoni">
