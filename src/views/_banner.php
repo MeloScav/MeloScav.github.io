1a456862322738d5fa62599a45d17151667eb893
MeloScav <meloscavezzoni@gmail.com>
2023-09-20 15:45:58 +0200

    Text change

--- src/views/_banner.php
+++ src/views/_banner.php
@@ -3,7 +3,7 @@
         <div class="main-banner">
             <div class="main-banner-content">
 
-                <p class="main-banner-sub-title text-md">Frontend web developer & UX / UI Designer</p>
+                <p class="main-banner-sub-title text-md">UX / UI Designer & Frontend web developer</p>
                 <h1 class="main-banner-title">Méloé Scavezzoni</h1>
 
                 <p class="main-banner-description text-lg">
@@ -29,22 +29,16 @@
                 </div>
 
             </div>
-            <img class="main-banner-illu" src="../assets/me-cartoon-illustration.svg"
-                alt="Illustration de Méloé Scavezzoni" />
+            <img class="main-banner-illu" src="../assets/me-cartoon-illustration.svg" alt="Illustration de Méloé Scavezzoni" />
         </div>
     </div>
 
     <div class="main-banner-waves-wrapper">
-        <svg class="main-banner-wave wave-1" data-animate-wave viewBox="0 0 1920 73" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path fill="#DCE8FB"
-                d="M1920 33C1689.1 -18.8235 1197.23 47.0527 969.622 47.1193C742.01 47.186 173.915 -47.9521 -0.000530051 34C0.00929529 67.5438 -9.28683e-05 73 -9.28683e-05 73L969.5 73L1920 73C1920 73 1920 49.9466 1920 33Z" />
+        <svg class="main-banner-wave wave-1" data-animate-wave viewBox="0 0 1920 73" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#DCE8FB" d="M1920 33C1689.1 -18.8235 1197.23 47.0527 969.622 47.1193C742.01 47.186 173.915 -47.9521 -0.000530051 34C0.00929529 67.5438 -9.28683e-05 73 -9.28683e-05 73L969.5 73L1920 73C1920 73 1920 49.9466 1920 33Z" />
         </svg>
-        <svg class="main-banner-wave wave-2" data-animate-wave viewBox="0 0 1920 92" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M0 53.764C338 62.1013 645 0.57891 962 0.00386357C1279 -0.571183 1412 63.2512 1920 53.764V92H0V53.764Z"
-                fill="#DCE8FB" fill-opacity="0.51" />
+        <svg class="main-banner-wave wave-2" data-animate-wave viewBox="0 0 1920 92" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 53.764C338 62.1013 645 0.57891 962 0.00386357C1279 -0.571183 1412 63.2512 1920 53.764V92H0V53.764Z" fill="#DCE8FB" fill-opacity="0.51" />
         </svg>
     </div>
 
