89e645b35e2228aa0a19deef92942cb4e53fe353
MeloScav <meloscavezzoni@gmail.com>
2024-03-05 10:18:20 +0100

    Add Polaroid project

---
+++ src/projects/polaroid3d/details.php
@@ -0,0 +1,32 @@
+<?php
+$title = "Projets | Polaroid 3D";
+$transparentMainNav = true;
+$projectsType = 'illustration';
+$isDetails = true;
+require($_SERVER['DOCUMENT_ROOT'] . '/src/data/_database.php');
+
+$currentProject = [];
+
+foreach ($PROJECTS as $project) {
+    if (isset($project['link']) && $project['link'] === str_replace("/src", "", $_SERVER['PHP_SELF'])) {
+        $currentProject = $project;
+    }
+}
+
+
+?>
+<?php ob_start(); ?>
+
+<?php include("../../views/projects-details/_banner_section.php"); ?>
+<div class="section-stack">
+    <?php include("../../views/projects-details/_presentation_section.php"); ?>
+    <?php include("../../views/projects-details/_colors_section.php"); ?>
+    <?php include("../../views/projects-details/_fonts_section.php"); ?>
+    <?php include("../../views/projects-details/_icons_section.php"); ?>
+    <?php include("../../views/projects-details/_design_section.php"); ?>
+    <?php include("../../views/_projects_slider_section.php"); ?>
+    <?php include("../../views/_call_to_action_section.php"); ?>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('../../views/_template.php'); ?>
