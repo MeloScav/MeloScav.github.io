14433665548acac3f1f76862ce2b1dfa5598bf32
MeloScav <meloscavezzoni@gmail.com>
2024-03-27 10:24:35 +0100

    Initialize banner and start style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import { Container } from "react-bootstrap";
+import TitleDecorated from "./components/title-decorated";
+import TextHighlighted from "./components/text-highlighted";
+
 export default function Home() {
-  return <p>Page d&apos;accueil</p>;
+  return (
+    <Container className="main-banner">
+      <div className="main-banner__content mw-1000 mx-auto">
+        <TitleDecorated className="text-center">
+          Méloé Scavezzoni
+        </TitleDecorated>
+        <p className="fs-xl mt-6">
+          <strong>
+            <span className="text-secondary">UX</span> /{" "}
+            <span className="text-primary">UI</span> designeuse
+          </strong>{" "}
+          et développeuse web frontend, mon objectif est de créer des designs au{" "}
+          <strong>pixel perfect</strong> qui reflètent parfaitement la vision du
+          client tout en maintenant une{" "}
+          <TextHighlighted className="fw-bold">
+            expérience utilisateur
+          </TextHighlighted>{" "}
+          optimale.
+        </p>
+      </div>
+    </Container>
+  );
 }
