40dbd5a65112a730823add07f292c321377d3286
MeloScav <meloscavezzoni@gmail.com>
2024-04-25 10:57:55 +0200

    Creation of projects database

---
+++ src/data/projects.ts
@@ -0,0 +1,43 @@
+import { IProject } from "@/types";
+
+// Replace ’ with &apos;
+
+export const projects = (
+  limit?: number,
+  order: "asc" | "des" | "random" = "des",
+  categoriesToExclude?: Array<string>
+): Array<IProject> => {
+  const projectsList = [
+    {
+      id: "audit-ux",
+      name: "Audit UX",
+      description:
+        "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
+      shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
+      thumbSrc: "/images/projects/audit-ux/thumb.jpg",
+      tags: ["Audit UX", "UX", "UI", "Design"],
+    },
+    {
+      id: "audit-ux1",
+      name: "Audit UX",
+      description:
+        "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
+      shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
+      thumbSrc: "/images/projects/audit-ux/thumb.jpg",
+      tags: ["Audit UX", "UX", "UI", "Design"],
+    },
+    {
+      id: "audit-ux2",
+      name: "Audit UX",
+      description:
+        "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
+      shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
+      thumbSrc: "/images/projects/audit-ux/thumb.jpg",
+      tags: ["Audit UX", "UX", "UI", "Design"],
+    },
+  ];
+
+  return projectsList;
+};
+
+// Replace ’ with &apos;
