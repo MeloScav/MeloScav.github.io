2e49eef9b5e588bf39be4f9ebdd740d82b6fa74c
MeloScav <meloscavezzoni@gmail.com>
2024-04-25 11:24:01 +0200

    Import spacer and recent projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import MainBanner from "./components/main-banner";
+import Spacer from "./components/spacer";
+import RecentProjects from "./components/recent-projects";
 
 export default function Home() {
-  return <MainBanner />;
+  return (
+    <>
+      <MainBanner />
+      <Spacer></Spacer>
+      <RecentProjects />
+      <span>Tatatatata</span>
+      <Spacer></Spacer>
+    </>
+  );
 }
