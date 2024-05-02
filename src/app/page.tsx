eab3a13a3ecff99faf82659bbaaceea7956ee765
MeloScav <meloscavezzoni@gmail.com>
2024-05-02 12:11:37 +0200

    Add See more projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import MainBanner from "./components/main-banner";
 import Spacer from "./components/spacer";
 import RecentProjects from "./components/recent-projects";
+import SeeMoreProjects from "./components/see-more-projects";
 
 export default function Home() {
   return (
     <>
       <MainBanner />
-      <Spacer></Spacer>
+      {/* <Spacer></Spacer> */}
       <RecentProjects />
+      <SeeMoreProjects />
       <span>Tatatatata</span>
       <Spacer></Spacer>
     </>
