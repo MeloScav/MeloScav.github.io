33e065a46a7fece71ead283e7cc5f3b7b74f0067
MeloScav <meloscavezzoni@gmail.com>
2024-05-02 12:07:46 +0200

    Add category

---
+++ src/data/categories.ts
@@ -0,0 +1,25 @@
+import { CategoryType, ICategory } from "@/types";
+
+export const categories: ICategory[] = [
+  {
+    id: CategoryType.DESIGN,
+    description:
+      "Ensemble de mes réalisations de design, analyse UX / UI, page web, web application, application mobile, etc.",
+    name: "Design",
+    thumbSrc: "/images/categories/design/thumb.png",
+    tags: ["UX / UI", "Design", "Figma"],
+  },
+  {
+    id: CategoryType.DEVELOPMENT,
+    description: "Ensemble de mes réalisations de développement.",
+    name: "Development",
+    thumbSrc: "",
+  },
+  {
+    id: CategoryType.ILLUSTRATION,
+    description:
+      "Ensemble de mes illustrations, créations de logo, icônes, modèles 3D, etc.",
+    name: "Illustration",
+    thumbSrc: "/images/categories/illustration/thumb.png",
+  },
+];
