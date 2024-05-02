68057f4b374397b16ad1cdbc492699d1027abc3a
MeloScav <meloscavezzoni@gmail.com>
2024-05-02 11:58:30 +0200

    Add category type

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,16 +1,17 @@
-import { IProject } from "@/types";
+import { CategoryType, IProject } from "@/types";
 
 // Replace ’ with &apos;
 
 export const projects = (
   limit?: number,
   order: "asc" | "des" | "random" = "des",
-  categoriesToExclude?: Array<string>
+  categoriesToExclude?: Array<keyof typeof CategoryType>
 ): Array<IProject> => {
   const projectsList = [
     {
       id: "audit-ux",
       name: "Audit UX",
+      category: CategoryType.DESIGN,
       description:
         "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
       shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
@@ -20,6 +21,7 @@ export const projects = (
     {
       id: "audit-ux1",
       name: "Audit UX",
+      category: CategoryType.DESIGN,
       description:
         "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
       shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
@@ -29,6 +31,7 @@ export const projects = (
     {
       id: "audit-ux2",
       name: "Audit UX",
+      category: CategoryType.DESIGN,
       description:
         "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
       shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,23 @@
+export enum CategoryType {
+  DESIGN = "DESIGN",
+  ILLUSTRATION = "ILLUSTRATION",
+  DEVELOPMENT = "DEVELOPMENT",
+}
+
 export interface IProject {
   id: string;
   name: string;
   description: string;
   shortDescription: string;
+  thumbSrc: string;
+  tags?: Array<string>;
+  category: keyof typeof CategoryType;
+}
+
+export interface ICategory {
+  id: keyof typeof CategoryType;
+  name: string;
+  description: string;
   thumbSrc: string;
   tags?: Array<string>;
 }
