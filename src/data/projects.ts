5026aafc8e8ddcb8f558fb4d2570768d56166c99
MeloScav <meloscavezzoni@gmail.com>
2024-10-15 09:16:34 +0200

    Refactor projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,46 +1,34 @@
 import { CategoryType, IProject } from "@/types";
 
-// Replace ’ with &apos;
-
-export const projects = (
-  limit?: number,
-  order: "asc" | "des" | "random" = "des",
-  categoriesToExclude?: Array<keyof typeof CategoryType>
-): Array<IProject> => {
-  const projectsList = [
-    {
-      id: "audit-ux",
-      name: "Audit UX",
-      category: CategoryType.DESIGN,
-      description:
-        "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
-      shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
-      thumbSrc: "/images/projects/audit-ux/thumb.jpg",
-      tags: ["Audit UX", "UX", "UI", "Design"],
-    },
-    {
-      id: "audit-ux1",
-      name: "Audit UX",
-      category: CategoryType.DESIGN,
-      description:
-        "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
-      shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
-      thumbSrc: "/images/projects/audit-ux/thumb.jpg",
-      tags: ["Audit UX", "UX", "UI", "Design"],
-    },
-    {
-      id: "audit-ux2",
-      name: "Audit UX",
-      category: CategoryType.DESIGN,
-      description:
-        "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
-      shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
-      thumbSrc: "/images/projects/audit-ux/thumb.jpg",
-      tags: ["Audit UX", "UX", "UI", "Design"],
-    },
-  ];
-
-  return projectsList;
-};
-
-// Replace ’ with &apos;
+export const projects: Array<IProject> = [
+  {
+    id: "audit-ux",
+    name: "Audit UX",
+    category: CategoryType.DESIGN,
+    description:
+      "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
+    shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
+    thumbSrc: "/images/projects/audit-ux/thumb.jpg",
+    tags: ["Audit UX", "UX", "UI", "Design"],
+  },
+  {
+    id: "audit-ux1",
+    name: "Audit UX",
+    category: CategoryType.DESIGN,
+    description:
+      "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
+    shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
+    thumbSrc: "/images/projects/audit-ux/thumb.jpg",
+    tags: ["Audit UX", "UX", "UI", "Design"],
+  },
+  {
+    id: "audit-ux2",
+    name: "Audit UX",
+    category: CategoryType.DESIGN,
+    description:
+      "Audit UX d&apos;une landing page d&apos;e-shop pour un magasin de thé avec une contre proposition designé",
+    shortDescription: "Audit UX d&apos;une landing page d&apos;e-shop.",
+    thumbSrc: "/images/projects/audit-ux/thumb.jpg",
+    tags: ["Audit UX", "UX", "UI", "Design"],
+  },
+];
