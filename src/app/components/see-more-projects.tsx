f655bbbc4fa3d7d6a85b178ef54f69387f3095dd
MeloScav <meloscavezzoni@gmail.com>
2024-10-15 09:09:03 +0200

    Add decoration icon

--- src/app/components/see-more-projects.tsx
+++ src/app/components/see-more-projects.tsx
@@ -1,12 +1,43 @@
-import React from "react";
-import { Container } from "react-bootstrap";
 import DecorationIcon from "./decorations/decoration-icon";
 import CardCategory from "../projects/card-category";
 import { categories } from "@/data/categories";
 
 const SeeMoreProjects = () => {
   return (
-    <Container className="py-section py-section--1">
+    <div className="position-relative">
+      <div className="decoration-icons-wrapper">
+        <DecorationIcon
+          iconId={"circle"}
+          className="text-secondary-light"
+          width="32px"
+          style={{ top: "39.65%", right: "102.77%" }}
+        />
+        <DecorationIcon
+          iconId={"hashtag"}
+          className="text-primary-light"
+          width="36px"
+          style={{ bottom: "104.7%", left: "59.76%" }}
+        />
+        <DecorationIcon
+          iconId={"star"}
+          className="text-secondary-light"
+          width="49px"
+          style={{ bottom: "101.22%", right: "5.14%" }}
+        />
+        <DecorationIcon
+          iconId={"triangle"}
+          className="text-primary-light"
+          width="44px"
+          style={{ bottom: "9.43%", right: "106.10%" }}
+        />
+        <DecorationIcon
+          iconId={"circle"}
+          className="text-primary-light"
+          width="32px"
+          style={{ bottom: "12.78%", left: "102.83%" }}
+        />
+      </div>
+
       <h2 className="pb-9 text-center text-black position-relative mw-1000 mx-auto">
         Voir plus de projets
         <DecorationIcon
@@ -25,7 +56,7 @@ const SeeMoreProjects = () => {
           <CardCategory key={category.id} category={category} />
         ))}
       </div>
-    </Container>
+    </div>
   );
 };
 
