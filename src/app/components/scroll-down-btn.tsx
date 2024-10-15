b5865e3e2eae2960df6fec716510586938cc8abc
MeloScav <meloscavezzoni@gmail.com>
2024-10-15 09:08:38 +0200

    Comment Gsap

--- src/app/components/scroll-down-btn.tsx
+++ src/app/components/scroll-down-btn.tsx
@@ -12,39 +12,39 @@ gsap.registerPlugin(useGSAP, ScrollTrigger);
 const ScrollDownBtn = () => {
   const scrollBox = useRef<HTMLDivElement>(null);
 
-  useGSAP(() => {
-    const scrollBoxElement = scrollBox.current;
+  // useGSAP(() => {
+  //   const scrollBoxElement = scrollBox.current;
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: scrollBoxElement,
-        toggleActions: "restart none reverse none",
-        start: "top 60%",
-        end: "bottom 10%",
-        scrub: 2,
-        // markers: true,
-      },
-    });
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: scrollBoxElement,
+  //       toggleActions: "restart none reverse none",
+  //       start: "top 60%",
+  //       end: "bottom 10%",
+  //       scrub: 2,
+  //       // markers: true,
+  //     },
+  //   });
 
-    tl.to(scrollBoxElement, {
-      y: "-100px",
-      rotation: 180,
-      ease: "power1.out",
-    })
-      .to(".scroll-text", { opacity: 0 })
-      .to(scrollBoxElement, {
-        y: "800px",
-        rotation: 260,
-        scale: "10",
+  //   tl.to(scrollBoxElement, {
+  //     y: "-100px",
+  //     rotation: 180,
+  //     ease: "power1.out",
+  //   })
+  //     .to(".scroll-text", { opacity: 0 })
+  //     .to(scrollBoxElement, {
+  //       y: "800px",
+  //       rotation: 260,
+  //       scale: "10",
 
-        ease: "power1.out",
-      })
-      .to(scrollBoxElement, {
-        rotation: 360,
-        scale: "80",
-        ease: "power1.out",
-      });
-  }, [scrollBox]);
+  //       ease: "power1.out",
+  //     })
+  //     .to(scrollBoxElement, {
+  //       rotation: 360,
+  //       scale: "80",
+  //       ease: "power1.out",
+  //     });
+  // }, [scrollBox]);
 
   return (
     <div className="main-banner__scroll">
